9b1addb9f07afd66a43ee28f3063db245113c1a9
hettling <hannes.hettling@gmail.com>
2017-11-17 17:44:09 +0100

    a

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+# deploy image to docker hub
+
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+
+if [[ -v TRAVIS_TAG ]]; then
+ 	DOCKER_TAG=testtag${TRAVIS_TAG}
+	docker push $DOCKER_IMAGE_NAME:$DOCKER_TAG
+	echo "Deployed image $DOCKER_IMAGE_NAME:$DOCKER_TAG to docker hub."   
+fi
+
+docker push $DOCKER_IMAGE_NAME:latest
+echo "Deployed image $DOCKER_IMAGE_NAME:latest to docker hub."   
+
+docker logout
