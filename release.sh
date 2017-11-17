55258775d1a880bb1979089b82da48fc6b844181
hettling <hannes.hettling@gmail.com>
2017-11-17 17:44:44 +0100

    qq

--- release.sh
+++ release.sh
@@ -1,1 +1,18 @@
-git tag -a v0.1 -m "my version 0.1"
+
+# semantic version tag as argument, MUST match /v\d+\.\d+\.\d+/ and MUST be at least
+# one minor increment above the current highest git tag version.
+TAGVERSION=$1
+if [ -z "$1" ]; then 
+	echo "Need varsion tag as argument. Must match /v\d+\.\d+\.\d+/"
+	exit 0;
+fi
+
+# set tag as travis environment variable
+travis env set TRAVIS_TAG v$TAGVERSION
+
+# make git tag and push
+git tag -a v$TAGVERSION -m "Release v$TAGVERSION"
+git push --tags
+
+# unset environment variable
+travis env unset TRAVIS_TAG
